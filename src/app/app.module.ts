1f2b983efeec59cba258ec4856f8aa60304276d5
Spiritbe4r <flywithmee20@gmail.com>
2022-07-15 23:57:59 -0500

    2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoAddComponent } from './todo/todo-add/todo-add.component';
+import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
+import { TodoItemComponent } from './todo/todo-item/todo-item.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoComponent } from './todo/todo.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
+import { appReducers } from './app.reducer';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoAddComponent,
+    TodoComponent,
+    TodoFooterComponent,
+    TodoItemComponent,
+    TodoListComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(appReducers),
+    // Note that you must instrument after importing StoreModule
+    StoreDevtoolsModule.instrument({
+      maxAge: 5
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
